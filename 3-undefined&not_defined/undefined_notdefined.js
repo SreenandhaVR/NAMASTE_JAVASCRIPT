6617348c5536e46084d0a12ca8136099b92d5c52
Sreenandha V R <sreenandhavr161@gmail.com>
2025-11-05 20:07:53 +0530

    Undefined and not defined

---
+++ 3-undefined&not_defined/undefined_notdefined.js
@@ -0,0 +1,26 @@
+// var a;
+// console.log(a);
+
+// console.log(a);
+// var a;
+// Here it will show undefined, because variable a is declared using var keyword
+
+// console.log(a);
+// var a = 7;
+// console.log(x);
+// Here it will show undefined, because of hoisting in JavaScript
+
+// var a;
+// console.log(a);
+// a = 7;
+// console.log(a);
+// Here it will show undefined first and then 7, because variable a is declared first and then assigned a value
+
+var a;
+console.log(a);
+ if(a===undefined){
+    console.log("a is undefined");
+ }
+ else{
+  console.log("a is not undefined");
+ }
